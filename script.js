ce39850486f3d43dce0c3406b9d11f851686f2a8
eugene dami <eugene.dami@yahoo.fr>
2024-05-03 23:34:21 +0200

    Add delete and mark as read options on books

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ const addButton = document.querySelector(".add");
 const libraryDisplay = document.querySelector(".main");
 const addBookForm = document.getElementById("addBookForm");
 const closeForm = document.querySelector(".close");
+let id = 1;
+
 
 // FUNCTIONS
 
 // Book constructor
-function Book(title, author, pages, read, coverUrl) {
+function Book(title, author, pages, read, coverUrl, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
   this.coverUrl = coverUrl;
+  this.id = id;
 }
-
 
 // Function to display/hide form on click
 formContainer.style.display = "none";
@@ -27,7 +29,9 @@ function displayLibrary() {
   libraryDisplay.innerHTML = "";
   myLibrary.forEach((book) => {
     let bookcard =
-    `<div class="book-card">
+    `<div class="book-card" data-id="${book.id}">
+    <img src="images/pencil.svg" alt="modify icon" class="modify">
+    <button class="delete">X</button>
     <div class="book-cover"><img src="${book.coverUrl}"></div>
     <div class="book-info">
     <div class="main-info">
@@ -44,8 +48,6 @@ function displayLibrary() {
   });
 }
 
-// EVENT LISTENERS
-
 // Function to toggle form visibility
 addButton.addEventListener("click", toggleForm);
 closeForm.addEventListener("click", toggleForm);
@@ -60,12 +62,47 @@ addBookForm.addEventListener("submit", function(event) {
   const read = document.getElementById("read").value;
   const coverUrl = document.getElementById("coverUrl").value;
   // Create book object
-  const book = new Book(title, author, pages, read, coverUrl);
+  const book = new Book(title, author, pages, read, coverUrl, id);
   // Add book to library
   myLibrary.push(book);
   // Clear form inputs
   addBookForm.reset();
   // Hide form
   formContainer.style.display = "none";
+  id += 1;
   displayLibrary();
+  deleteBook();
+  modifyBook();
 });
+
+// Function for book deletion
+function deleteBook() {
+  const deleteButtons = document.querySelectorAll(".delete");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      const bookId = parseInt(button.parentElement.dataset.id);
+      const bookIndex = myLibrary.findIndex((book) => book.id === bookId);
+      if (bookIndex !== -1) {
+        myLibrary.splice(bookIndex, 1);
+        displayLibrary();
+      }
+    })
+  })
+}
+
+// Function for marking book as read
+function modifyBook() {
+  const modifyButtons = document.querySelectorAll(".modify");
+  modifyButtons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      const bookId = parseInt(button.parentElement.dataset.id);
+      const bookIndex = myLibrary.findIndex((book) => book.id === bookId);
+      if (bookIndex !== -1) {
+        myLibrary[bookIndex].read = "read";
+        displayLibrary();
+        console.log("yo");
+      }
+    })
+  })
+}
+
